1ce06916531437e9880d5b86cb1f2770db389611
AinoHukkanen <n1huai00@students.oamk.fi>
2022-04-24 13:09:49 +0300

    Kaikenlaista

--- js/hukkanen.js
+++ js/hukkanen.js
@@ -1,5 +1,69 @@
+
+
+
 function kaupunkivastaus() {
     
-        document.getElementById("result1").innerHTML = "Vastaus on väärin. Oikea vastaus on Oslo."
+let kysymys1 = document.getElementById("1").value;
+let correct = 0;
 
+if (kysymys1 == "Oslo") {
+        correct++;
+        document.getElementById("result1").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Sinulla on nyt ansaittuna " + correct + " pistettä.";
+        
+} else 
+        document.getElementById("result1").innerHTML = "Vastauksesi on väärin. Norjan pääkaupunki on Oslo. "
 }
+function rahayksikkovastaus() {
+        let kysymys2 = document.getElementById("textbox").value;
+        let correct = 0;
+
+        if (kysymys2 !== "punta") {
+                document.getElementById("result2").innerHTML = "Vastauksesi on väärin. Iso-Britannian rahayksikkö on punta.";
+                
+        } else
+        correct++;
+        document.getElementById("result2").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Sinulla on nyt ansaittuna " + correct + " pistettä.";
+        
+}
+
+function lippuvastaus() {
+        let kysymys3 = document.getElementById("3").value;
+        let correct = 0;
+
+        if (kysymys3 == "Vatikaanivaltio") {
+                correct++;
+                document.getElementById("result3").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Sinulla on nyt ansaittuna " + correct + " pistettä.";
+                
+        } else
+        document.getElementById("result3").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä lippu on Vatikaanivaltion lippu."
+        
+}
+
+function nahtavyysvastaus() {
+        let kysymys4 = document.getElementById("4").value;
+        let correct = 0;
+
+        if (kysymys4 == "Barcelona") {
+                correct++;
+                document.getElementById("result4").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Sinulla on nyt ansaittuna " + correct + " pistettä.";
+                
+        } else
+        document.getElementById("result4").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä nähtävyys on Espanjan Barcelonassa sijaitseva Sagrada Familia."
+        
+}
+function riikavastaus() {
+        let kysymys5 = document.getElementById("5").value;
+        let correct = 0;
+
+        if (kysymys5 == "Latvia") {
+                correct++;
+                document.getElementById("result5").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Sinulla on nyt ansaittuna " + correct + " pistettä.";
+                
+        } else
+        document.getElementById("result5").innerHTML = "Vastauksesi on väärin. Riika on Latvian pääkaupunki"
+        
+}
+
+
+
+
