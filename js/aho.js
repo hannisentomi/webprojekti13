88773abae43498f4f9e50ab61ad0b1e162642b3b
Henrik Aho <you@example.com>
2022-04-26 18:05:52 +0300

    js muutoksia

--- js/aho.js
+++ js/aho.js
@@ -9,3 +9,39 @@ if (oikein){
         'Väärin meni';
     }
 }
+
+function vastaus1() {
+    let oikein = document.getElementById('a3').checked;
+    if (oikein){
+        document.getElementById('demo2').innerHTML =
+        'oikein meni';
+        }else {
+            document.getElementById('demo2').innerHTML =
+            'Väärin meni';
+        }
+    }
+
+    function vastaus2() {
+        let oikein = document.getElementById('a4').checked;
+        if (oikein){
+            document.getElementById('demo3').innerHTML =
+            'oikein meni';
+            }else {
+                document.getElementById('demo3').innerHTML =
+                'Väärin meni';
+            }
+        }
+
+        function vastaus3() {
+            let oikein = document.getElementById('c5').checked;
+            if (oikein){
+                document.getElementById('demo4').innerHTML =
+                'oikein meni';
+                }else {
+                    document.getElementById('demo4').innerHTML =
+                    'Väärin meni';
+                }
+            }
+    
+
+    
