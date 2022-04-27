3dbe7a39c2c963e41375ca1afc269e605d4e9c88
AinoHukkanen <n1huai00@students.oamk.fi>
2022-04-27 13:50:51 +0300

    Sisältöä lisätty

--- js/hukkanen.js
+++ js/hukkanen.js
@@ -12,16 +12,22 @@ function kaupunkivastaus() {
         
         document.getElementById("result1").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Norjan pääkaupunki on tosiaankin Oslo. Sinulla on nyt ansaittuna " + correct + " pistettä.";
         img.src = "../images/hukkanen_images/owl-dab.png"
-        document.getElementById("result1").appendChild(img);
+        document.getElementById("owl1").appendChild(img);
 
     } else if (document.getElementById("1b").checked) {
         document.getElementById("result1").innerHTML = "Vastauksesi on väärin. Norjan pääkaupunki on Oslo. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl1").appendChild(img);
 
     } else if (document.getElementById("1c").checked) {
             document.getElementById("result1").innerHTML = "Vastauksesi on väärin. Norjan pääkaupunki on Oslo. Sinulla on nyt  " + correct + " pistettä."
+            img.src = "../images/hukkanen_images/sad.png"
+            document.getElementById("owl1").appendChild(img);
     }
     else if (document.getElementById("1d").checked) {
         document.getElementById("result1").innerHTML = "Vastauksesi on väärin. Norjan pääkaupunki on Oslo. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl1").appendChild(img);
     } else {
         document.getElementById("result1").innerHTML = "Sinun täytyy valita joku vaihtoehdoista."
     }
@@ -37,12 +43,14 @@ function rahayksikkovastaus() {
         correct++;
         document.getElementById("result2").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Punta on Iso-Britannian rahayksikkö. Sinulla on nyt ansaittuna " + correct + " pistettä.";
         img.src = "../images/hukkanen_images/owl-dab.png"
-        document.getElementById("result2").appendChild(img);
+        document.getElementById("owl2").appendChild(img);
     } else if (kysymys2 == "") {
-        document.getElementById("result2").innerHTML = "Sinun täytyy antaa vastaus."
+        document.getElementById("result2").innerHTML = "Sinun täytyy antaa vastaus kysymykseen."
         }
      else {
         document.getElementById("result2").innerHTML = "Vastauksesi on väärin. Iso-Britannian rahayksikkö on punta. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl2").appendChild(img);
     }
 }
 
@@ -53,13 +61,19 @@ function lippuvastaus() {
         correct++;
         document.getElementById("result3").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Kuvassa näkyvä lippu on Vatikaanivaltion lippu.  Sinulla on nyt ansaittuna " + correct + " pistettä.";
         img.src = "../images/hukkanen_images/owl-dab.png"
-        document.getElementById("result3").appendChild(img);
+        document.getElementById("owl3").appendChild(img);
     } else if (document.getElementById("3a").checked) {
         document.getElementById("result3").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä lippu on Vatikaanivaltion lippu. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl3").appendChild(img);
     } else if (document.getElementById("3b").checked) {
         document.getElementById("result3").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä lippu on Vatikaanivaltion lippu. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl3").appendChild(img);
     } else if (document.getElementById("3d").checked) {
         document.getElementById("result3").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä lippu on Vatikaanivaltion lippu. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl3").appendChild(img);
     } else {
         document.getElementById("result3").innerHTML = "Sinun täytyy valita joku vaihtoehdoista."
     }
@@ -72,13 +86,19 @@ function nahtavyysvastaus() {
         correct++;
         document.getElementById("result4").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Kuvassa näkyvä nähtävyys on Sagrada Familia, joka sijaitsee Barcelonassa. Sinulla on nyt ansaittuna " + correct + " pistettä.";
         img.src = "../images/hukkanen_images/owl-dab.png"
-        document.getElementById("result4").appendChild(img);
+        document.getElementById("owl4").appendChild(img);
     } else if (document.getElementById("4a").checked) {
         document.getElementById("result4").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä nähtävyys on Espanjan Barcelonassa sijaitseva Sagrada Familia. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl4").appendChild(img);
     } else if (document.getElementById("4c").checked) {
         document.getElementById("result4").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä nähtävyys on Espanjan Barcelonassa sijaitseva Sagrada Familia. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl4").appendChild(img);
     } else if (document.getElementById("4d").checked) {
         document.getElementById("result4").innerHTML = "Vastauksesi on väärin. Kuvassa näkyvä nähtävyys on Espanjan Barcelonassa sijaitseva Sagrada Familia. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl4").appendChild(img);
     } else {
         document.getElementById("result4").innerHTML = "Sinun täytyy valita joku vaihtoehdoista."
     }
@@ -90,15 +110,44 @@ function riikavastaus() {
         correct++;
         document.getElementById("result5").innerHTML = "Vastauksesi on oikein, onneksi olkoon! Latvian pääkaupunki on Riika. Sinulla on nyt ansaittuna " + correct + " pistettä.";
         img.src = "../images/hukkanen_images/owl-dab.png"
-        document.getElementById("result5").appendChild(img);
+        document.getElementById("owl5").appendChild(img);
     } else if (document.getElementById("5b").checked) {
         document.getElementById("result5").innerHTML = "Vastauksesi on väärin. Riika on Latvian pääkaupunki. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl5").appendChild(img);
     } else if (document.getElementById("5c").checked) {
         document.getElementById("result5").innerHTML = "Vastauksesi on väärin. Riika on Latvian pääkaupunki. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl5").appendChild(img);
     } else if (document.getElementById("5d").checked) {
         document.getElementById("result5").innerHTML = "Vastauksesi on väärin. Riika on Latvian pääkaupunki. Sinulla on nyt  " + correct + " pistettä."
+        img.src = "../images/hukkanen_images/sad.png"
+        document.getElementById("owl5").appendChild(img);
     } else {
         document.getElementById("result5").innerHTML = "Sinun täytyy valita joku vaihtoehdoista."
     }
+var totalscore = ["Et saanut yhtään pistettä tai pöllöä. Joudut kertaamaan paljon maantietoa", "Sait yhden pisteen ja pöllön. Se on ihan hyvä tulos, mutta sinun kannattaisi vielä kerrata maantietoa.", "Sait kaksi pistettä ja kaksi pöllä. Se on ihan hyvä tulos, mutta sinun kannattaisi vielä kerrata maantietoa.", "Sait kolme pistettä ja kolme pöllöä. Hienoa työtä! Jotain asioita kuitenkin kannattaa vielä kerrata.", "Sait neljä pistettä ja pöllöä, mahtava saldo!", "Sait kaikki kysymykset oikein ja täydet pisteet sekä kaikki viisi pöllöä! Taidat olla haka maantiedossa"];
+
+    if (correct == 0) {
+
+        document.getElementById("finalscore").innerHTML = totalscore[0];
+    }
+    if (correct == 1) {
+        
+        document.getElementById("finalscore").innerHTML = totalscore[1];
+    } if (correct == 2) {
+        
+        document.getElementById("finalscore").innerHTML = totalscore[2];
+    } if (correct == 3) {
+       
+        document.getElementById("finalscore").innerHTML = totalscore[3];
+    } if (correct == 4) {
+        
+        document.getElementById("finalscore").innerHTML = totalscore[4];
+    } if (correct == 5) {
+       
+        document.getElementById("finalscore").innerHTML = totalscore[5];
+    }
+
 
 }
